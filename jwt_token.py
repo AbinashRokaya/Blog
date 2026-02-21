a5247503ed30647b6c7b2a90af08d3ae0111c835
Abirokaya <rokayaabinash9@gmail.com>
2026-02-21 14:28:26 +0545

    jwt

--- jwt_token.py
+++ jwt_token.py
@@ -6,7 +6,7 @@ import os
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
 
 def create_access_token(subject: str,
